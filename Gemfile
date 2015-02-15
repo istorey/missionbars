d31fd3cf20cf7fc30eb2157ed50e7afd0bf6ef56
istorey <wrightstoreys@gmail.com>
2015-02-15 12:50:27 -0500

    user test created

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
-gem 'rspec'
+
+gem 'rspec-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+
+describe User do
+	it "is valid with an email"
+	it "is valid with a password"
+	it "is invalid with a duplicate email"
+	it "is invalid without an email address"
+	it "is invalid without a password"
+end
