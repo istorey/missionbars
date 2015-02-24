18881399e8e78af621565d44d69c7ae367a37068
istorey <wrightstoreys@gmail.com>
2015-02-24 14:20:32 -0500

    wall controller

---
+++ app/controllers/walls_controller.rb
@@ -0,0 +1,4 @@
+class WallsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'walls/new'
+
   get 'posts/new'
 
   devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'} 
