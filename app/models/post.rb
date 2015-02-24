339c336777b5b50d133f561146a154e73a1a02c4
istorey <wrightstoreys@gmail.com>
2015-02-24 14:17:58 -0500

    changed relations in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
 	has_attached_file :image, styles: { large: "300x300", medium: "150x150", thumb: "100x100#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	belongs_to :user
+	belongs_to :wall
 end
--- app/models/wall.rb
+++ app/models/wall.rb
@@ -1,2 +1,4 @@
 class Wall < ActiveRecord::Base
+	belongs_to :users
+	has_many :posts, through: :users
 end
